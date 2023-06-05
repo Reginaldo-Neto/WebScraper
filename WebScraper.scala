31b7d5839b476ee9afaf64011fc5d78ad3275f54
Gabriela Paola Sereniski <gabsereniski@gmail.com>
2023-06-05 16:05:50 -0300

    infinitos links

--- WebScraper.scala
+++ WebScraper.scala
@@ -1,13 +1,16 @@
 import org.jsoup.Jsoup
 import scala.collection.JavaConverters._
+import scala.io.Source
+import java.io.PrintWriter
 
 object WebCrawler {
   def main(args: Array[String]): Unit = {
-    val searchTerm = "moonlight" // Termo de busca desejado
-    val baseUrl = "https://www.imdb.com/find/?q=moonlight" // URL base do site a ser rastreado
+    val searchTerm = "Moonlight" // Termo de busca desejado
+    val baseUrl = "https://www.rottentomatoes.com/search?search=" // URL base do site a ser rastreado
+    val searchUrl = baseUrl + searchTerm
     val visitedPages = collection.mutable.Set[String]() // Páginas já visitadas
 
-    crawlPage(baseUrl, searchTerm, visitedPages)
+    crawlPage(searchUrl, searchTerm, visitedPages)
   }
 
   def crawlPage(url: String, searchTerm: String, visitedPages: collection.mutable.Set[String]): Unit = {
@@ -18,18 +21,42 @@ object WebCrawler {
 
         // Verificar se o termo de busca está presente no conteúdo da página
         if (doc.body().text().toLowerCase.contains(searchTerm.toLowerCase)) {
-            val document = Jsoup.connect(url).get()
+            // val document = Jsoup.connect(url).get()
+
+            // println(s"Mencao encontrada em $url.")
+
+            val links = doc.select("a[href]") // Selecionar todos os links na página
+            val matchingLinks = links.asScala.filter(link => link.text().toLowerCase.contains(searchTerm.toLowerCase)) // Filtrar os links que contêm o termo de busca
+              
+
+            val finalLinks = matchingLinks.filterNot(link =>
+              link.text().contains("reviews")
+            )
+
+            finalLinks.foreach { link =>
+              val movieUrl = link.absUrl("href") // Extrair o URL do link
+              println(movieUrl)
+              crawlPage(movieUrl, searchTerm, visitedPages) // Chamar a função crawlPage para visitar a página associada ao link
+            }
+
+            val htmlContent = doc.html()
+            val filepath = "output.txt"
+            val writer = new PrintWriter(filepath)
+            writer.println(htmlContent)
+            writer.close()
+            println(s"Conteudo salvo em $filepath")
+              
             
             // Extrair informações relevantes da página
-            val movieName = document.select(".sc-afe43def-1 fDTGTb").text()
-            val description = document.select(".sc-2eb29e65-1 goRLhJ").text()
-            val rating = document.select(".sc-bde20123-1 iZlgcd").text()
+            // val movieName = doc.select("sc-afe43def-0 esVJhx").text()
+            // val description = doc.select("sc-2eb29e65-1 goRLhJ").text()
+            // val rating = doc.select("sc-bde20123-1 iZlgcd").text()
 
-            // Imprimir as informações extraídas
-            println("Nome do filme: " + movieName)
-            println("Descrição: " + description)
-            println("Avaliação: " + rating + "/10")
-            println("-----------------------------------")
+            // // Imprimir as informações extraídas
+            // println("Nome do filme: " + movieName)
+            // println("Descrição: " + description)
+            // println("Avaliação: " + rating + "/10")
+            // println("-----------------------------------")
         }
 
         // Extrair links das páginas e recursivamente visitar cada uma delas
--- build.sbt
+++ build.sbt
@@ -5,9 +5,9 @@ version := "1.0"
 scalaVersion := "2.13.5"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.6.17",
-  "com.typesafe.akka" %% "akka-stream" % "2.6.17",
-  "com.typesafe.akka" %% "akka-http" % "10.2.6",
+  // "com.typesafe.akka" %% "akka-actor" % "2.6.17",
+  // "com.typesafe.akka" %% "akka-stream" % "2.6.17",
+  // "com.typesafe.akka" %% "akka-http" % "10.2.6",
   "org.jsoup" % "jsoup" % "1.14.3"
 )
 
