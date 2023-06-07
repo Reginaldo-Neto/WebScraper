2fae328aef49423a274345fc93573b601e05ed90
gabsereniski <gabsereniski@gmail.com>
2023-06-06 22:00:09 -0300

    #7 - Paralelização do scraping por site

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,38 +8,38 @@ import scala.io.Source
 import scala.io.StdIn
 import java.io.PrintWriter
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 object Main {
-    def main(args: Array[String]): Unit = {
-      println("Digite o termo de busca:")
-      val searchTerm = scala.io.StdIn.readLine()
+  def main(args: Array[String]): Unit = {
+    println("Digite o termo de busca:")
+    val searchTerm = scala.io.StdIn.readLine()
 
-      val imdbFuture: Future[List[(String, String, String, String, String)]] = Future {
-          scrapIMDB(searchTerm)
-      }
-
-      val rottenFuture: Future[List[(String, String, String, String, String)]] = Future {
-          scrapRotten(searchTerm)
-      }
-
-      val combinedFuture: Future[(List[(String, String, String, String, String)], List[(String, String, String, String, String)])] =
-          for {
-              imdbData <- imdbFuture
-              rottenData <- rottenFuture
-          } yield (imdbData, rottenData)
-
-      val combinedResult = Await.result(combinedFuture, 90.seconds)
-
-      val imdbResult = combinedResult._1
-      val rottenResult = combinedResult._2
-
-      // Fazer algo mais interessante com as informações coletadas
-      imdbResult.foreach(println)
-      println("---------------------------------------------")
-      println("---------------------------------------------")
-      rottenResult.foreach(println)
+    val imdbFuture: Future[List[(String, String, String, String, String)]] = Future {
+      scrapIMDB(searchTerm)
     }
+
+    val rottenFuture: Future[List[(String, String, String, String, String)]] = Future {
+      scrapRotten(searchTerm)
+    }
+
+    val combinedFuture: Future[(List[(String, String, String, String, String)], List[(String, String, String, String, String)])] =
+      for {
+        imdbData <- imdbFuture
+        rottenData <- rottenFuture
+      } yield (imdbData, rottenData)
+
+    val processedFuture: Future[Unit] = combinedFuture.map { case (imdbData, rottenData) =>
+      // Fazer algo mais interessante com as informações coletadas
+      imdbData.foreach(println)
+      println("---------------------------------------------")
+      println("---------------------------------------------")
+      rottenData.foreach(println)
+    }
+
+    // Esperar pela conclusão do processamento
+    Await.result(processedFuture, 90.seconds)
+  }
 }
